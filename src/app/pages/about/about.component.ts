491247028fc7bf535766b67af0adbf93523d5bfa
Luciano Murruni <luciano.murruni@gmail.com>
2022-12-28 08:50:47 +0100

    feat: new configuration and copy

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -6,13 +6,59 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      about works!
-    </p>
-  `,
-  styles: [
-  ]
-})
-export class AboutComponent {
+    <div class="relative z-10 flex items-center overflow-hidden bg-white dark:bg-gray-800">
+      <div class="container relative flex px-6 py-16 mx-auto">
+        <div class="relative z-20 flex flex-col sm:w-2/3 lg:w-2/5">
+          <span class="w-20 h-2 mb-12 bg-gray-800 dark:bg-white"> </span>
+          <h1
+            class="flex flex-col text-6xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-8xl dark:text-white">
+            Introduction
+          </h1>
+          <p class="text-sm text-gray-700 sm:text-base dark:text-white">
+            Raffle games are an exciting way to bring an extra element of fun to your events. An online raffle game is a
+            great tool for increasing engagement and driving donations. With an online raffle game, you can easily reach
+            a larger audience, create a unique and memorable experience, and increase donations for your cause. How It
+            Works
+          </p>
 
-}
+          <h1
+            class="flex flex-col text-6xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-8xl dark:text-white">
+            How It Works
+          </h1>
+          <p class="text-sm text-gray-700 sm:text-base dark:text-white">
+            To host an online raffle game, you’ll need to register your event with a third-party service that has the
+            ability to host online raffles. Once you’ve registered your event, you’ll be able to create a custom raffle
+            where participants can purchase tickets to enter. You’ll also be able to set the number of tickets
+            available, the price of each ticket, and any additional conditions or restrictions. Once the tickets are
+            purchased, participants can then enter the raffle. The winner will be chosen at random and will be notified
+            via email. Depending on the service you choose, you may also have the option to have the winner announced
+            publicly on a live stream or other platform.
+          </p>
+
+          <h1
+            class="flex flex-col text-6xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-8xl dark:text-white">
+            Benefits
+          </h1>
+          <p class="text-sm text-gray-700 sm:text-base dark:text-white">
+            An online raffle game is a great way to engage your audience and increase donations. It’s a fun and
+            interactive way to draw while also raising funds for your cause. It’s also relatively easy to set up and
+            manage, allowing you to focus on other aspects of your event. Plus, since tickets can be purchased
+            electronically, it’s a great way to make donations more accessible to those who may not be able to attend
+            your event in person.
+          </p>
+
+          <h1
+            class="flex flex-col text-6xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-8xl dark:text-white">
+            Conclusion
+          </h1>
+          <p class="text-sm text-gray-700 sm:text-base dark:text-white">
+            An online raffle game is a great way to add an extra layer of fun and excitement to your event. It’s an easy
+            way to engage your audience, increase donations, and create a memorable experience.
+          </p>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [],
+})
+export class AboutComponent {}
--- src/app/pages/players/players.component.ts
+++ src/app/pages/players/players.component.ts
@@ -12,17 +12,22 @@ import { PlayersService } from 'src/app/shared/services/players.service';
     <div class="container max-w-3xl px-4 mx-auto sm:px-8">
       <div class="py-8">
         <div class="flex flex-row justify-between w-full mb-1 sm:mb-0">
-          <h2 class="text-2xl leading-tight">
-              Users
-          </h2>
+          <h2 class="text-2xl leading-tight">Users</h2>
           <div class="text-end">
-            <form class="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
-                <div class=" relative ">
-                  <input type="text" id="&quot;form-subscribe-Filter" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name"/>
-                </div>
-                <button class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-                    Filter
-                </button>
+            <form
+              class="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
+              <div class=" relative ">
+                <input
+                  type="text"
+                  id='"form-subscribe-Filter'
+                  class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  placeholder="name" />
+              </div>
+              <button
+                class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+                type="submit">
+                Filter
+              </button>
             </form>
           </div>
         </div>
@@ -30,23 +35,33 @@ import { PlayersService } from 'src/app/shared/services/players.service';
           <div class="inline-block min-w-full overflow-hidden rounded-lg shadow" *ngIf="players$ | async as players">
             <table class="min-w-full leading-normal">
               <thead>
-                  <tr>
-                      <th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                          UserId
-                      </th>
-                      <th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                          Name
-                      </th>
-                      <th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                          Deck
-                      </th>
-                      <th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                          Edit
-                      </th>
-                      <th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                          Delete
-                      </th>
-                  </tr>
+                <tr>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                    UserId
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                    Deck
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                    Edit
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
+                    Delete
+                  </th>
+                </tr>
               </thead>
 
               <tbody>
@@ -66,19 +81,33 @@ import { PlayersService } from 'src/app/shared/services/players.service';
                   </td>
                   <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
                     <a (click)="editPlayer(player)">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-blue-400" fill="none"
-                          viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-6 h-6 text-blue-400"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                       </svg>
                     </a>
                   </td>
                   <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
                     <a (click)="deletePlayer(player)">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-400" fill="none"
-                          viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-6 h-6 text-red-400"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                       </svg>
                     </a>
                   </td>
@@ -90,11 +119,9 @@ import { PlayersService } from 'src/app/shared/services/players.service';
       </div>
     </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class PlayersComponent implements OnInit {
-
   players$!: Observable<Player[]>;
   _playerService = inject(PlayersService);
 
@@ -103,7 +130,7 @@ export class PlayersComponent implements OnInit {
   }
 
   editPlayer(player: Player) {
-    console.log('click on: ',player)
+    console.log('click on: ', player);
   }
   deletePlayer(player: Player) {
     if (confirm(`Sicuro di voler eliminare ${player.name}`)) {
