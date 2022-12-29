8e517f2105094c8b760ce014d9fc662aa9493877
Luciano Murruni <luciano.murruni@gmail.com>
2022-12-30 00:11:07 +0100

    fix: add mobile menu and footer url

--- src/app/pages/new-game/new-game.component.ts
+++ src/app/pages/new-game/new-game.component.ts
@@ -32,14 +32,12 @@ import { PlayersService, RaffleCollection } from 'src/app/shared/services/player
                   <button
                     type="button"
                     (click)="createNew()"
-                    class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                    class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg ">
                     Start Now
                   </button>
                 </div>
               </div>
-              <div>
-              {{currentGame$ | async | json}}
-              </div>
+
             </div>
           </div>
         </div>
