fb18c63cc1a3064cb7d567350d4adac9b4bf4c7b
Luciano Murruni <luciano.murruni@gmail.com>
2023-03-30 10:34:29 +0200

    feat: move join add join button for non-auth user

--- apps/estrai.me/src/app/pages/index/index.component.ts
+++ apps/estrai.me/src/app/pages/index/index.component.ts
@@ -46,6 +46,11 @@ import { AuthService } from '@auth';
                 class="px-4 py-2 mr-4 text-white uppercase bg-red-600 border-2 border-transparent rounded-lg text-md hover:bg-red-800">
                 Login to start
               </a>
+              <a
+                [routerLink]="['/game/join']"
+                class="px-4 py-2 text-red-600 uppercase bg-transparent border-2 border-red-600 rounded-lg dark:text-white hover:bg-red-600 hover:text-white text-md">
+                Join
+              </a>
             </div>
           </ng-template>
         </div>
