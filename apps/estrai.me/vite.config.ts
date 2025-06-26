9c172bf728d9564fb7146d83a658af53b59ae168
lucianomurr <luciano.murruni@gmail.com>
2025-06-26 13:31:27 +0200

    feat: update vite and vercel deploy

--- apps/estrai.me/vite.config.ts
+++ apps/estrai.me/vite.config.ts
@@ -26,6 +26,9 @@ export default defineConfig(({ mode }) => {
         prerender: {
           routes: ['/'],
         },
+        nitro: {
+          preset: 'vercel',
+        },
       }),
 
       nxViteTsPaths(),
