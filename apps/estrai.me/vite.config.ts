ff00ac415901a449b4b0cfcb7eb8d5742d6b2d92
lucianomurr <luciano.murruni@gmail.com>
2025-06-26 13:19:12 +0200

    fix: update prerendered routes

--- apps/estrai.me/vite.config.ts
+++ apps/estrai.me/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig(({ mode }) => {
         ssr: false,
         static: true,
         prerender: {
-          routes: [],
+          routes: ['/'],
         },
       }),
 
